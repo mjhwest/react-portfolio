a77e364be4b09b22559f3457699208bb7cba65b2
mjhwest89 <mjhwest89@gmail.com>
2022-01-27 03:23:53 +1030

    finished header section

--- react-port/src/App.js
+++ react-port/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import TParticles from "react-tsparticles";
+
+// not working
+// import Particles from "react-tsparticles";
 
 function App() {
   return (
     <>
-  <TParticles />
    <Navbar /> 
     <Header />
 </>
@@ -15,3 +16,46 @@ function App() {
 }
 
 export default App;
+
+
+
+/* <Particles 
+
+options = {{
+  particles: {
+    number: {
+      value:30, 
+      density: {
+        enable: true,
+        value_area:900
+      }
+    }
+  }
+}} */
+
+// options={{
+//   particles: {
+//     color: {
+//       value: "#ffffff",
+//     },
+//   },
+//   links: {
+//     color: "#ffffff",
+//           distance: 150,
+//           enable: true,
+//           opacity: 0.5,
+//           width: 1,
+//   },
+//   number: {
+//     density: {
+//       enable: true,
+//       area: 800,
+//     },
+//     value: 40, 
+//   }
+
+// }}
+
+//neeed to close this off here </> 
+
+
