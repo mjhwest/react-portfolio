69ddccbce35fd3342e7bdeb24bfde42ad3e99da0
mjhwest89 <mjhwest89@gmail.com>
2022-01-27 01:21:58 +1030

    more work on header

--- react-port/src/App.js
+++ react-port/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-<Navbar /> 
+    <>
+    <Particles />
+     <Navbar /> 
+    <Header />
+</>
   );
 }
 
