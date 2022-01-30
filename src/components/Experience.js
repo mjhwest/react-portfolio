4f477f5f2a29638e54d8049fd273ad08d20bc1ed
mjhwest89 <mjhwest89@gmail.com>
2022-01-31 02:24:12 +1030

    added smooth function

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Experience() {
-  return <div className="experience">
+  return <div id="experience" className="experience">
       <div className="d-flex justify-content-center my-5"> 
       <h1> experience </h1>
       </div>
