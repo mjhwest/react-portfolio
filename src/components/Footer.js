2c71cb33a4f13b260d89b8d6adf2136328658dea
mjhwest89 <mjhwest89@gmail.com>
2022-01-31 02:24:37 +1030

    added function css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import {
+    FacebookShareButton, 
+    FacebookIcon,
+    TwitterShareButton, 
+    TwitterIcon, 
+    RedditIcon,
+    RedditShareButton,
+    LinkedinShareButton,
+    LinkedinIcon,
+    WhatsappShareButton,
+    WhatsappIcon
+    
+} from "react-share"; 
 
 export const Footer = () => {
   return( 
@@ -14,11 +27,61 @@ export const Footer = () => {
                         <p>michaelwest.webdev@gmail.com </p> 
                     </div>
                 </div>
+                <div className="col-lg-3 col-md-2 col-sm-6">
+                <div className="row">
+                        <div className="col">
+                            <a className="footer-nav"> Home</a>
+                            <br/>
+                            <a className="footer-nav"> About Me </a>
+                            <br/>
+                            <a className="footer-nav"> Experience</a>
+                            <br/>
+                        </div>
+                        <div className="col">
+                            <a className="footer-nav"> Portfolio</a>
+                            <br/>
+                            <a className="footer-nav"> Contact Me </a>
+                            <br/>
+                            <a className="footer-nav"> GitHub </a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-5 col-md-5 col-sm-6 align-items-center">
+                <div className="d-flex justify-content-center"> 
+                <FacebookShareButton
+                url={"https://github.com/mjhwest"}
+                quote= {" An Aspiring Full Stack Developer"}>
+                    <FacebookIcon className="mx-3" size={36}/>
+                </FacebookShareButton>
+                <TwitterShareButton
+                url={"https://github.com/mjhwest"}
+                quote= {" An Aspiring Full Stack Developer"}>
+                    <TwitterIcon className="mx-3" size={36}/>
+                </TwitterShareButton>
+                <RedditShareButton
+                url={"https://github.com/mjhwest"}
+                quote= {" An Aspiring Full Stack Developer"}>
+                    <RedditIcon className="mx-3" size={36}/>
+                </RedditShareButton>
+                <LinkedinShareButton
+                url={"https://github.com/mjhwest"}
+                quote= {" An Aspiring Full Stack Developer"}>
+                    <LinkedinIcon className="mx-3" size={36}/>
+                </LinkedinShareButton>
+                <WhatsappShareButton
+                url={"https://github.com/mjhwest"}
+                quote= {" An Aspiring Full Stack Developer"}>
+                    <WhatsappIcon className="mx-3" size={36}/>
+                </WhatsappShareButton>
+                </div>
+                <p className="pt-3 text-center">
+                    Copyright&copy;
+                    {new Date().getFullYear()}&nbsp; Michael West 
+                </p>
             </div>
         </div>
-
     </div>
-
+</div>
   )
 };
 
