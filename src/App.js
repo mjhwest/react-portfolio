145c08cc3d8cf4f1fc58e2e38a65aa0f09f0bdd5
mjhwest89 <mjhwest89@gmail.com>
2022-02-01 00:58:11 +1030

    push to save

--- src/App.js
+++ src/App.js
@@ -8,14 +8,8 @@ import Portfolio from './components/Portfolio';
 import Contacts from "./components/Contacts";
 import Footer from "./components/Footer"
 
-
-
-
-
-
 function App() {
  
-  
 
   return (
     <>
