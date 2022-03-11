e0dc806aa38ce7bc10d9022d76cef6f203ee64cb
mjhwest89 <mjhwest89@gmail.com>
2022-03-11 11:29:42 +1030

    changed to expereicne section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,11 +7,11 @@ function Experience() {
       </div>
       <div className="container experience-wrapper"> 
       <div className="timeline-block timeline-block-left">
-          <div className="marker"> </div>
-              <div className="timeline-content">
+          {/* <div className="marker"> </div> */}
+              {/* <div className="timeline-content">
                   <h3> 1989 </h3>
                   <p>Born  </p>
-              </div>
+              </div> */}
           </div>
       <div className="timeline-block timeline-block-right">
           <div className="marker"> </div>
